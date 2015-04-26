817c78fb660a581f2a9a06e1ec4e62020afb6f58
Cory <cory.morales@colorado.edu>
2015-04-26 00:40:38 -0600

    Added header file

--- Deck.h
+++ Deck.h
@@ -1,5 +1,26 @@
 #ifndef DECK_H
 #define DECK_H
 
+struct Card{
+    std::string number;
+    int suit;
+
+    Card(){};
+
+    Card(std::string innumber, int insuit){
+        number = innumber;
+        suit = insuit;
+    }
+};
+
+class Deck{
+public:
+    Deck();
+    ~Deck();
+    void shuffleDeck();
+
+private:
+    Card decklist[52];
+};
 
 #endif // DECK_H
