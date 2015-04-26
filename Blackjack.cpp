22af6416abb45ca78f900167d139e6a6b9f71978
BryceGriebenow <brgr0407@colorado.edu>
2015-04-26 13:27:37 -0600

    added some comments

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -6,49 +6,49 @@ using namespace std;
 int main(){
     string players, operation, ai;
     int Players, Spot, AI, op, hoomans;
-    playahs:
+    playahs://identifier to go back to if the input for the next line is unacceptable
     cout<<"How many players will there be? (5 max)"<<endl;
     getline(cin,players);
     Players=stoi(players);
-    if(Players<0||Players>5){
+    if(Players<0||Players>5){//checks to see if the amount of players is acceptable
         cout<<"Not a valid number."<<endl;
         goto playahs;
     }
-    ai_players:
+    ai_players://identifier to go back to if the input for the next line is unacceptable
     cout<<"How many players will be AI? (0-"<<Players<<")"<<endl;
     getline(cin,ai);
     AI=stoi(ai);
-    if(AI<0||AI>Players){
+    if(AI<0||AI>Players){//checks to see if the amount of AI is acceptable
         cout<<"Not a valid number."<<endl;
         goto ai_players;
     }
-    Deck d(Players);
+    Deck d(Players);//constructs the deck to have the correct amount of players
     if(AI!=0){
-        hoomans=Players-AI;
+        hoomans=Players-AI;//how many actual humans are playing
         for(int i=0;i<hoomans;i++){
-            Spot=rand()%Players-1;
-            d.player[Spot].ai=false;
+            Spot=rand()%Players-1;//will randomize spot as if at an actual blackjack table
+            d.players[Spot].ai=false;
         }
     }
-    else{
+    else{//if no AI, then make all spots human
         for(int i=0;i<Players;i++){
-            d.player[i].ai=false;
+            d.players[i].ai=false;
         }
     }
-    d.DealCards(Players);
+    d.DealCards(Players);//deals the cards
     for(int i=0;i<Players;i++){
-        bool turn=true;
-        if(!d.player[i].ai){
-            while(turn){
-                cout<<"Player "<<i+1<<", your hand is "<<d.player[i].hand<<".\nWould you like to\n1.Hit\n2.Stay\n3.See what the other hands are"<<endl;
+        bool turn=true;//resets for every player
+        if(!d.players[i].ai){//for a human player
+            while(turn){//will stay until they bust or win or stay
+                cout<<"Player "<<i+1<<", your hand is "<<d.players[i].hand<<".\nWould you like to\n1.Hit\n2.Stay\n3.See what the other hands are"<<endl;
                 getline(cin,operation);
                 op=stoi(operation);
                 switch(op){
                     case 1:
-                        turn=d.hit(i);
+                        turn=d.hit(i);//if they bust or win it will end their turn
                         break;
                     case 2:
-                        turn=false;
+                        turn=false;//ends turn
                         break;
                     case 3:
                         d.peek();
@@ -58,10 +58,10 @@ int main(){
                 }
             }
         }
-        else{
+        else{//for AI
             while(turn){
-                cout<<"Player "<<i+1<<"'s hand is "<<d.player[i].hand<<endl;
-                if(d.player[i].sum()<17){
+                cout<<"Player "<<i+1<<"'s hand is "<<d.players[i].hand<<endl;
+                if(d.players[i].sum<17){//will automatically hit if the sum is less than 17
                     cout<<"Player "<<i+1<<" hits."<<endl;
                     turn=d.hit(i);
                 }
@@ -72,7 +72,7 @@ int main(){
             }
         }
     }
-    d.dealer();
-    d.declare();
+    d.dealer();//plays for the dealer
+    d.declare();//declares the winner
     return 0;
 }
